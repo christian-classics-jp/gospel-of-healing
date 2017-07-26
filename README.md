11f392e65363a802fb52c6328bbeae85d82d51e6
FujiHaruka <fujiharuka@realglobe.jp>
2017-07-26 21:36:56 +0900

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# 神癒の福音 | Ａ・Ｂ・シンプソン
+
++ [原文PDF](https://www.cmalliance.org/resources/archives/downloads/simpson/the-gospel-of-healing.pdf)
+
+聖書の引用は新改訳第三版による。
---
+++ build.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+/**
+ * build ja.md
+ */
+const fs = require('fs')
+const { join } = require('path')
+
+process.chdir(join(__dirname))
+
+let dirs = fs.readdirSync('src')
+for (let dir of dirs) {
+  let srcPath = `src/${dir}/both.md`
+  let jaPath = `src/${dir}/jp.md`
+
+  if (!fs.existsSync(srcPath)) {
+    continue
+  }
+  let text = fs.readFileSync(srcPath).toString()
+  let texts = text.split('\n').map(str => str.trim())
+  if (texts.length < 3) {
+    continue
+  }
+  let jaSentences = texts.filter(
+    (text, i) => !/^[a-zA-Z]/.test(text)
+  ).map(
+    (text) => text + '\n'
+  )
+  let ja = jaSentences.join('')
+  fs.writeFileSync(jaPath, ja)
+}
